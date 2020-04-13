6d6be945ab0a852ea4f71b83cd5831f6b3e6ddef
Armen Juhl <armenjuhl@gmail.com>
2020-04-13 05:53:18 -0700

    restructure 2

--- manage.py
+++ manage.py
@@ -1,24 +1,53 @@
 import os
 # import pytest
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+from app.app import api
 
 from app.main import create_app
+from app.namespace import ns
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 
 app.app_context().push()
 
 
-def run():
-    app.run()
+app = Flask(__name__)
+
+db = SQLAlchemy()
+
+# logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
+# logging.config.fileConfig(logging_conf_path)
+# log = logging.getLogger(__name__)
 
 
-# def test():
-#     """Runs the unit tests."""
-#     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-#     result = unittest.TextTestRunner(verbosity=2).run(tests)
-#     if result.wasSuccessful():
-#         return 0
-#     return 1
+def configure_app(flask_app):
+    flask_app.config['SERVER_NAME'] = 'localhost:5000'
+    # flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+    # flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
+    flask_app.config['RESTPLUS_VALIDATE'] = True
+    flask_app.config['RESTPLUS_MASK_SWAGGER'] = False
+    flask_app.config['ERROR_404_HELP'] = False
 
-if __name__ == '__main__':
-    app.run()
+
+def initialize_app(flask_app):
+    configure_app(flask_app)
+
+    blueprint = Blueprint('api', __name__, url_prefix='/api')
+    api.init_app(blueprint)
+    api.add_namespace(ns)
+    flask_app.register_blueprint(blueprint)
+
+    db.init_app(flask_app)
+
+
+def main():
+    initialize_app(app)
+    # log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()
